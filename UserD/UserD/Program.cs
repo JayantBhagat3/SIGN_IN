a2349a604c71b641b0ddddd789cc9be3b3241c3d
Rounak Jaiswal <rounakkjaiswal@gmail.com>
2022-03-09 13:43:16 +0530

    WebApi

---
+++ UserD/UserD/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using UserD.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UserDContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UserDContext")));
+// Add services to the container.
+//Access - Control - Allow - Origin: http://localhost:8080;
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
